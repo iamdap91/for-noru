d191a3e0c662fdfe961e060c7e9289f2a0580243
3sam3 <iamdap91@gmail.com>
2022-10-21 02:49:32 +0900

    feat: cli

--- apps/admin-e2e/src/e2e/app.cy.ts
+++ apps/admin-e2e/src/e2e/app.cy.ts
@@ -7,7 +7,7 @@ describe('admin', () => {
     // Custom command example, see `../support/commands.ts` file
     cy.login('my-email@something.com', 'myPassword');
 
-    // Function helper example, see `../support/app.po.ts` file
+    // Function helper example, see `../support/commands.po.ts` file
     getGreeting().contains('Welcome admin');
   });
 });
---
+++ apps/cli/src/commands/commands.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+
+import { CommandsService } from './commands.service';
+
+@Module({
+  imports: [],
+  providers: [CommandsService],
+})
+export class CommandsModule {}
