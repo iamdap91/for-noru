9151ef8e71b52c8c5ac9a789f51e7ed75e5209bd
3sam3 <iamdap91@gmail.com>
2022-10-23 05:41:24 +0900

    feat: engine

--- libs/engine/src/A001/A001.service.ts
+++ libs/engine/src/A001/A001.service.ts
@@ -7,17 +7,51 @@ import { EPS2097 } from '../../../common/src/geo-transcoder';
 @Injectable()
 export class A001Service {
   async restaurant({ name, address, coordinates }: EngineParam, page: Page) {
-    console.log(name, address, coordinates);
-
     const url = this.figureUrl(name, coordinates);
+    console.log(name, address, coordinates);
+    console.log(url);
+
+    const listInterceptor = this.interceptRequest(
+      'https://map.naver.com/v5/api/search?caller=pcweb&query=',
+      page
+    );
     await page.goto(url);
     await page.waitForNavigation();
+    const response: any = await listInterceptor;
 
-    console.log(1);
+    const restaurant = response.result.place.list.find(
+      (item) => item.name === name
+    );
+
+    if (!restaurant) {
+      return;
+    }
+
+    const detailInterceptor = this.interceptRequest(
+      `https://map.naver.com/v5/api/sites/summary/${restaurant.id}`,
+      page
+    );
+    await page.goto(`${NAVER_MAP_URL}/${name}/place/${restaurant.id}`);
+    const detail = await detailInterceptor;
+
+    return detail;
+  }
+
+  async interceptRequest(url: string, page: Page) {
+    return new Promise(async (resolve) => {
+      await page.on('response', async (response) => {
+        const request = response.request();
+        if (request.url().includes(url)) {
+          const json = await response.json();
+          resolve(json);
+        }
+      });
+    });
   }
 
   figureUrl(name: string, coordinates: [number, number]) {
     let url = `${NAVER_MAP_URL}/${name}`;
+
     const [origX, origY] = coordinates;
     if (Boolean(origX && origY)) {
       const [x, y] = EPS2097.toEPSG3857(coordinates);
--- libs/engine/src/factories/browser-factory.ts
+++ libs/engine/src/factories/browser-factory.ts
@@ -1,19 +1,43 @@
-import { Browser, Page } from 'puppeteer';
+import { Browser, Page, ResourceType } from 'puppeteer';
 import UserAgent from 'user-agents';
 import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import { BrowserOptionInterface } from '../interfaces';
 
+// const DEFAULT_OPTIONS = { args: ['--no-sandbox'] };
+const TYPES_TO_BLOCK: ResourceType[] = [
+  'image',
+  'media',
+  'other',
+  'font',
+  'stylesheet',
+];
+
 export class BrowserFactory {
   static async createBrowser(
     options: BrowserOptionInterface
   ): Promise<Browser> {
-    return await puppeteer.use(StealthPlugin()).launch(options);
+    const { fastMode, ...launchOptions } = options;
+
+    const browser = await puppeteer.use(StealthPlugin()).launch(options);
+
+    if (fastMode) {
+      const [page] = await browser.pages();
+      await page.setRequestInterception(true);
+      page.on('request', (req) => {
+        const resourceType = req.resourceType();
+        TYPES_TO_BLOCK.includes(resourceType) ? req.abort() : req.continue();
+      });
+    }
+
+    return browser;
   }
 
   static async getPage(browser: Browser): Promise<Page> {
     const [page] = await browser.pages();
-    await page.setUserAgent(new UserAgent().random().toString());
+    await page.setUserAgent(
+      new UserAgent({ platform: 'Win32' }).random().toString()
+    );
 
     return page;
   }
