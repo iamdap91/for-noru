f41f178868d45c6c46ee18d86d7e654b65733c46
3sam3 <iamdap91@gmail.com>
2022-10-23 01:38:53 +0900

    feat: engine

--- libs/engine/src/A001/A001.service.ts
+++ libs/engine/src/A001/A001.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { Page } from 'puppeteer';
+import { NAVER_MAP_URL } from '../constants';
 
 @Injectable()
 export class A001Service {
   async restaurant(name: string, address: string, page: Page) {
-    console.log('restaurant scrape');
+    console.log(name, address);
+    await page.goto(`${NAVER_MAP_URL}/${name}`);
+    await page.waitForNavigation();
+
+    console.log(1);
   }
 }
--- libs/engine/src/constants.ts
+++ libs/engine/src/constants.ts
@@ -1,1 +1,3 @@
 export const WITH_BROWSER_META_DATA = 'meta:with_browser';
+
+export const NAVER_MAP_URL = 'https://map.naver.com/v5/search';
