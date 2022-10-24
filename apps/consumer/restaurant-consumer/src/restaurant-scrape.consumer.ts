4751e81a86e0d2893549238957636ce4f17ac814
3sam3 <iamdap91@gmail.com>
2022-10-24 14:10:15 +0900

    feat: queue consumer

--- apps/consumer/restaurant-consumer/src/restaurant-scrape.consumer.ts
+++ apps/consumer/restaurant-consumer/src/restaurant-scrape.consumer.ts
@@ -1,8 +1,11 @@
 import { Process, Processor } from '@nestjs/bull';
 import { DoneCallback, Job } from 'bull';
+import { RestaurantsService } from '@gong-gu/backend/restaurants';
 
 @Processor('restaurants')
 export class RestaurantScrapeConsumer {
+  constructor(private readonly service: RestaurantsService) {}
+
   @Process({ concurrency: 3 })
   async run({ data }: Job, done: DoneCallback) {
     console.log(data);
