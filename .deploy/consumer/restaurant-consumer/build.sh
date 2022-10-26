83cbc93e68cd979d369a7dcb5d095fa2830ebd98
3sam3 <iamdap91@gmail.com>
2022-10-27 03:18:22 +0900

    feat: docker builder

--- .deploy/consumer/restaurant-consumer/build.sh
+++ .deploy/consumer/restaurant-consumer/build.sh
@@ -14,3 +14,4 @@ fi
 TAG="$(git rev-parse HEAD)"
 export TAG
 docker-compose -f docker-compose-build.yaml build build
+docker tag for-noru/consumer/restaurant:$TAG for-noru/consumer/restaurant:$latest
