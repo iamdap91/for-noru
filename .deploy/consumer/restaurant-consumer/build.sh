c79e11507862c59b49e5e7bdee9973b1068c62ab
3sam3 <iamdap91@gmail.com>
2022-10-27 13:39:47 +0900

    feat: docker build

--- .deploy/consumer/restaurant-consumer/build.sh
+++ .deploy/consumer/restaurant-consumer/build.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-# need aws-cli and aws configure
-# https://docs.aws.amazon.com/ko_kr/cli/latest/userguide/cli-chap-welcome.html
-
-if [ -f .env ]
-then
-  export $(cat .env | sed 's/#.*//g' | xargs)
-else
-  echo ".env FILE을 만들어주세요."
-  exit 0
-fi
-
 TAG="$(git rev-parse HEAD)"
 export TAG
+
 docker-compose -f docker-compose-build.yaml build build
 docker tag for-noru/consumer/restaurant:$TAG for-noru/consumer/restaurant:latest
